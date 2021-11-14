d33e542a67c5a759688cef55840efbf4473be453
DESKTOP-A22BOPU\Nosel <itleson4793@gmail.com>
2021-11-14 17:10:22 +0700

    update ui

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-
+export class DashboardComponent implements OnInit, AfterViewChecked {
+  @ViewChild('widgetsContent', { read: ElementRef }) widgetsContent: ElementRef<any>;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewChecked(): void {
+    setTimeout(() => {
+      this.scrollRight();
+    }, 500);
+  }
+  scrollRight(): void {
+    this.widgetsContent.nativeElement.scrollTo({ left: (this.widgetsContent.nativeElement.scrollLeft + 5000), behavior: 'smooth' });
   }
 
 }
