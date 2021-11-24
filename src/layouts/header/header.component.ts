7634b46b360374082e5fa2caceb2a1a67af83ad1
DESKTOP-A22BOPU\Nosel <itleson4793@gmail.com>
2021-11-24 17:30:07 +0700

    update ui

--- src/layouts/header/header.component.ts
+++ src/layouts/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  isShow = false;
   constructor() { }
 
   ngOnInit(): void {
@@ -16,5 +16,6 @@ export class HeaderComponent implements OnInit {
   scroll(id) {
     let el = document.getElementById(id);
     el!.scrollIntoView({ behavior: 'smooth' });
+    this.isShow = false;
   }
 }
