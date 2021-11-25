0309db3eb7db827afe57154027503e5881afb927
fibonacci998 <fibonacci998@gmai.com>
2021-11-25 10:20:58 +0700

    add file robot and site map

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,17 @@ set -xe
 
 #Login to registry
 docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
-
+if [ ${CI_COMMIT_REF_NAME} = "develop" ]
+then
+    echo 'This is develop branch'
+    cp ./src/robots/robots-akachains.txt ./src/robots.txt
+    cp ./src/robots/sitemap-akachains.txt ./src/sitemap.uml
+elif [ ${CI_COMMIT_REF_NAME} = "uat" ]
+then
+    echo 'This is uat branch'
+    cp ./src/robots/robots-aura.txt ./src/robots.txt
+    cp ./src/robots/sitemap-aura.txt ./src/sitemap.uml
+fi
 #Build and push image
 docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
 docker push ${CONTAINER_RELEASE_IMAGE}
