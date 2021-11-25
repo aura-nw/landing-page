aa48dda0d848158cca8a9a01a019e0cf5f822a80
fibonacci998 <fibonacci998@gmai.com>
2021-11-25 10:24:04 +0700

    list file robot

--- ci/build.sh
+++ ci/build.sh
@@ -6,6 +6,8 @@ docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
 if [ ${CI_COMMIT_REF_NAME} = "develop" ]
 then
     echo 'This is develop branch'
+    pwd
+    ls -al
     cp ./src/robots/robots-akachains.txt ./src/robots.txt
     cp ./src/robots/sitemap-akachains.txt ./src/sitemap.uml
 elif [ ${CI_COMMIT_REF_NAME} = "uat" ]
