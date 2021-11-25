9a785a4aec5cc17e9f25e864b881f8743071afcf
fibonacci998 <fibonacci998@gmai.com>
2021-11-25 10:30:54 +0700

    update script build

--- ci/build.sh
+++ ci/build.sh
@@ -6,15 +6,13 @@ docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
 if [ ${CI_COMMIT_REF_NAME} = "develop" ]
 then
     echo 'This is develop branch'
-    pwd
-    ls ./src
     cp ./src/robots/robots-akachains.txt ./src/robots.txt
-    cp ./src/robots/sitemap-akachains.txt ./src/sitemap.uml
+    cp ./src/robots/sitemap-akachains.uml ./src/sitemap.uml
 elif [ ${CI_COMMIT_REF_NAME} = "uat" ]
 then
     echo 'This is uat branch'
     cp ./src/robots/robots-aura.txt ./src/robots.txt
-    cp ./src/robots/sitemap-aura.txt ./src/sitemap.uml
+    cp ./src/robots/sitemap-aura.uml ./src/sitemap.uml
 fi
 #Build and push image
 docker build -t ${CONTAINER_RELEASE_IMAGE} -f Dockerfile .
