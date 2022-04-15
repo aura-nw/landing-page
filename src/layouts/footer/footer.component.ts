acce485b24d3f32884607267a36752c6d4053d08
letrieuthien <ltthien001@gmail.com>
2022-04-15 08:29:09 +0700

    add insight-aura link

--- src/layouts/footer/footer.component.ts
+++ src/layouts/footer/footer.component.ts
@@ -6,17 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  formData: {email: string | undefined} = {email: ''};
-  email: string | undefined;
-  submitted = false;
   constructor() { }
 
   ngOnInit(): void {}
 
-  onSubmitCTAForm() {
-    this.submitted = true;
-    console.log(this.formData.email);
-  }
   scroll(id, position: 'nearest' | 'end' | 'start' | 'center') {
     const el = document.getElementById(id);
     el?.scrollIntoView({ behavior: 'smooth', block: position });
