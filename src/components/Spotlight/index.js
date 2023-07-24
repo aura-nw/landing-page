5ffdbd0d968b58cc82fe834af91ec51b86439164
imhson <phamhoangson99@gmail.com>
2023-07-24 09:08:02 +0700

    change seekhype link

--- src/components/Spotlight/index.js
+++ src/components/Spotlight/index.js
@@ -18,37 +18,34 @@ const Wrap = styled.div`
 `
 export default function Spotlight() {
     return (
-        <Container>
-            <Wrap>
-                <Card backdrop={Backdrop1} logo={AuraLogo} onClick={() => window.open("https://aurascan.io/")}>
-                    <p className="title">Aura Scan</p>
-                    <p className="description">The next generation blockchain explorer for Aura Network.</p>
-                    <p className="text__color">Experience →</p>
-                </Card>
-                <Card backdrop={Backdrop1} logo={PyxisLogo} onClick={() => window.open("https://pyxis.aura.network/")}>
-                    <p className="title">Pyxis Safe</p>
-                    <p className="description">Multi-signature and fine-grain access control asset management tool.</p>
-                    <p className="text__color">Experience →</p>
-                </Card>
-                <Card
-                    backdrop={Backdrop1}
-                    logo={HoroscopeLogo}
-                    onClick={() => window.open("https://horoscope.aura.network")}>
-                    <p className="title">Horoscope</p>
-                    <p className="description">An indexing service to provide real-time data for Aura ecosystem.</p>
-                    <p className="text__color">Experience →</p>
-                </Card>
-                <Card backdrop={Backdrop2} logo={SeekHypeLogo} onClick={() => window.open("https://beta.seekhype.ai/")}>
-                    <p className="title">SeekHYPE</p>
-                    <p className="description">A personalized NFT marketplace powered by AI.</p>
-                    <p className="text__color">Experience →</p>
-                </Card>
-                <Card backdrop={Backdrop3} logo={HaloTradeLogo} onClick={() => undefined}>
-                    <p className="title">Halotrade</p>
-                    <p className="description">The first DEX built on Aura Network to swap, farm and earn.</p>
-                    <p className="text__color">Coming soon</p>
-                </Card>
-            </Wrap>
-        </Container>
+      <Container>
+        <Wrap>
+          <Card backdrop={Backdrop1} logo={AuraLogo} onClick={() => window.open('https://aurascan.io/')}>
+            <p className='title'>Aura Scan</p>
+            <p className='description'>The next generation blockchain explorer for Aura Network.</p>
+            <p className='text__color'>Experience →</p>
+          </Card>
+          <Card backdrop={Backdrop1} logo={PyxisLogo} onClick={() => window.open('https://pyxis.aura.network/')}>
+            <p className='title'>Pyxis Safe</p>
+            <p className='description'>Multi-signature and fine-grain access control asset management tool.</p>
+            <p className='text__color'>Experience →</p>
+          </Card>
+          <Card backdrop={Backdrop1} logo={HoroscopeLogo} onClick={() => window.open('https://horoscope.aura.network')}>
+            <p className='title'>Horoscope</p>
+            <p className='description'>An indexing service to provide real-time data for Aura ecosystem.</p>
+            <p className='text__color'>Experience →</p>
+          </Card>
+          <Card backdrop={Backdrop2} logo={SeekHypeLogo} onClick={() => window.open('https://beta.seekhype.io/')}>
+            <p className='title'>SeekHYPE</p>
+            <p className='description'>A personalized NFT marketplace powered by AI.</p>
+            <p className='text__color'>Experience →</p>
+          </Card>
+          <Card backdrop={Backdrop3} logo={HaloTradeLogo} onClick={() => undefined}>
+            <p className='title'>Halotrade</p>
+            <p className='description'>The first DEX built on Aura Network to swap, farm and earn.</p>
+            <p className='text__color'>Coming soon</p>
+          </Card>
+        </Wrap>
+      </Container>
     )
 }
