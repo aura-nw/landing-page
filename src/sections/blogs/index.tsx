b3b60acc21a592f2fe13a457eeb90d15dd10db55
hoangndm <mhoang.9699@gmail.com>
2024-02-03 23:05:55 +0700

    fix layout

--- src/sections/blogs/index.tsx
+++ src/sections/blogs/index.tsx
@@ -7,12 +7,12 @@ function Blogs() {
     const sliderRef = useRef<any>();
     return (
         <div className="flex flex-col gap-8 items-center xl:mt-10 xl:w-full">
-            <div className="flex w-full justify-center">
-                <div className="flex flex-col gap-4 items-center px-10 xl:px-0 xl:w-full xl:items-start">
+            <div className="flex w-full justify-center lg:px-6 xl:px-0">
+                <div className="flex flex-col gap-4 items-center px-10 lg:px-0 lg:w-full lg:items-start">
                     <span className="text-light-yellow font-normal text-center xl:text-start xl:text-xl">Aura's Blog</span>
                     <span className="text-white text-center font-ppmori-semibold text-[28px] font-semibold leading-9">Discover the future of Web3</span>
                 </div>
-                <div className="hidden xl:flex">
+                <div className="hidden lg:flex">
                     <SwiperButton onClickPrev={() => sliderRef.current?.slidePrev()} onClickNext={() => sliderRef.current?.slideNext()} />
                 </div>
             </div>
