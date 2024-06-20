e286c2befc656bd23190b5ab19141a65b1734251
DESKTOP-APEQLA2\NOSEL <sonln4793@gmail.com>
2024-06-20 17:39:40 +0700

    ud symbol

--- src/app/get-aura/deposit.tsx
+++ src/app/get-aura/deposit.tsx
@@ -5,7 +5,13 @@ import depositBingx from "@/assets/images/deposit_bingx.jpg";
 import depositGate from "@/assets/images/deposit_gate.jpg";
 import depositMexc from "@/assets/images/deposit_mexc.jpg";
 import Image from "next/image";
-import { useAccount, useSendTransaction, useBalance, useWaitForTransactionReceipt, BaseError } from "wagmi";
+import {
+  useAccount,
+  useSendTransaction,
+  useBalance,
+  useWaitForTransactionReceipt,
+  BaseError,
+} from "wagmi";
 import TableHistory from "./table";
 import { stringToHex, parseEther, parseUnits, formatUnits } from "viem";
 import { useForm, Controller } from "react-hook-form";
@@ -25,10 +31,16 @@ interface TableItemProps {
 
 function Deposit() {
   const [tutType, setTutType] = useState<string>("");
-  const { sendTransaction, data: hash, isPending, error } = useSendTransaction();
-  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
-    hash,
-  });
+  const {
+    sendTransaction,
+    data: hash,
+    isPending,
+    error,
+  } = useSendTransaction();
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    });
 
   const {
     handleSubmit,
@@ -44,6 +56,7 @@ function Deposit() {
     address: account.address,
   });
   const _amount = formatUnits(BigInt(balance?.data?.value || 0), 18);
+  const _symbol = balance?.data?.symbol;
 
   const addMaxAmount = () => {
     if (_amount && Number(_amount) && Number(_amount) > 0) {
@@ -83,7 +96,8 @@ function Deposit() {
   };
 
   const getActivityHistory = (fromAddress: string) => {
-    const url = "https://cex.staging.aura.network/public/DepositService/deposits";
+    const url =
+      "https://cex.staging.aura.network/public/DepositService/deposits";
 
     const request: AxiosRequestConfig = {
       url: url,
@@ -93,13 +107,15 @@ function Deposit() {
       },
     };
 
-    const res: Promise<AxiosResponse<DepositHistory[]>> = axios.request(request);
+    const res: Promise<AxiosResponse<DepositHistory[]>> =
+      axios.request(request);
     return res
       .then((response) => response.data)
       .then((res) => {
         if (res?.length > 0) {
           const mappedList = res?.map((item) => {
-            const status = item?.status === "completed" ? "success" : item?.status;
+            const status =
+              item?.status === "completed" ? "success" : item?.status;
             return {
               txTime: item.created_at,
               evmTxHash: item.incoming_tx_hash,
@@ -116,7 +132,9 @@ function Deposit() {
       });
   };
 
-  const [activityHistories, setActivityHistories] = useState<TableItemProps[]>([]);
+  const [activityHistories, setActivityHistories] = useState<TableItemProps[]>(
+    []
+  );
   useEffect(() => {
     if (account?.address) {
       getActivityHistory(account?.address?.toLowerCase() || "");
@@ -130,10 +148,16 @@ function Deposit() {
   return (
     <div className="main-container sub-container flex flex-col">
       <div className="flex flex-col">
-        <div className="introduce-title">Let’s deposit some AURA to your desired CEX below::</div>
+        <div className="introduce-title">
+          Let’s deposit some AURA to your desired CEX below::
+        </div>
         <div className="flex gap-8 items-center justify-center mt-6 partner">
           <div
-            className={tutType === "bingx" ? "active partner-button cursor-pointer" : "partner-button cursor-pointer"}
+            className={
+              tutType === "bingx"
+                ? "active partner-button cursor-pointer"
+                : "partner-button cursor-pointer"
+            }
             onClick={() => {
               setTutType("bingx");
             }}
@@ -141,7 +165,11 @@ function Deposit() {
             <Image src={bingx} alt="bingx" height={24} />
           </div>
           <div
-            className={tutType === "gateio" ? "active partner-button cursor-pointer" : "partner-button cursor-pointer"}
+            className={
+              tutType === "gateio"
+                ? "active partner-button cursor-pointer"
+                : "partner-button cursor-pointer"
+            }
             onClick={() => {
               setTutType("gateio");
             }}
@@ -149,7 +177,11 @@ function Deposit() {
             <Image src={gateio} alt="gate.io" height={24} />
           </div>
           <div
-            className={tutType === "mexc" ? "active partner-button cursor-pointer" : "partner-button cursor-pointer"}
+            className={
+              tutType === "mexc"
+                ? "active partner-button cursor-pointer"
+                : "partner-button cursor-pointer"
+            }
             onClick={() => {
               setTutType("mexc");
             }}
@@ -161,12 +193,29 @@ function Deposit() {
 
       {tutType != "" && (
         <div className="info flex gap-20 mt-15" style={{ marginTop: "60px" }}>
-          {tutType === "bingx" && <Image src={depositBingx} alt="bingx" height={depositBingx?.height} />}
-          {tutType === "gateio" && <Image src={depositGate} alt="gateio" height={depositGate?.height} />}
-          {tutType === "mexc" && <Image src={depositMexc} alt="mexc" height={depositMexc?.height} />}
+          {tutType === "bingx" && (
+            <Image
+              src={depositBingx}
+              alt="bingx"
+              height={depositBingx?.height}
+            />
+          )}
+          {tutType === "gateio" && (
+            <Image
+              src={depositGate}
+              alt="gateio"
+              height={depositGate?.height}
+            />
+          )}
+          {tutType === "mexc" && (
+            <Image src={depositMexc} alt="mexc" height={depositMexc?.height} />
+          )}
           <div className="flex flex-col width">
             <div className="flex flex-col">
-              <div className="text-tutorial mb-6">First, go to the Deposit section on your BingX account, and select AURA to retrieve the deposit address.</div>
+              <div className="text-tutorial mb-6">
+                First, go to the Deposit section on your BingX account, and
+                select AURA to retrieve the deposit address.
+              </div>
               <form onSubmit={handleSubmit(onSubmit)} className="form-custom">
                 <label className="form-label">
                   Enter <span className="orange">Deposit address </span>here
@@ -185,10 +234,19 @@ function Deposit() {
                           message: "Invalid address",
                         },
                       }}
-                      render={({ field }) => <input {...field} placeholder="Enter your deposit address from CEX" />}
+                      render={({ field }) => (
+                        <input
+                          {...field}
+                          placeholder="Enter your deposit address from CEX"
+                        />
+                      )}
                     />
                   </div>
-                  {errors.address && <div className="form-error">{errors.address.message?.toString()}</div>}
+                  {errors.address && (
+                    <div className="form-error">
+                      {errors.address.message?.toString()}
+                    </div>
+                  )}
                 </div>
                 <label className="form-label">Enter amount of AURA</label>
 
@@ -205,18 +263,45 @@ function Deposit() {
                           message: "Amount must be a positive number",
                         },
                       }}
-                      render={({ field }) => <input type="text" placeholder="Amount" {...field} onChange={handleChange} />}
+                      render={({ field }) => (
+                        <input
+                          type="text"
+                          placeholder="Amount"
+                          {...field}
+                          onChange={handleChange}
+                        />
+                      )}
                     />
-                    <button type="button" className="pr-4" onClick={() => addMaxAmount()}>
+                    <button
+                      type="button"
+                      className="pr-4"
+                      onClick={() => addMaxAmount()}
+                    >
                       max
                     </button>
                   </div>
-                  {errors.amount && <div className="form-error">{errors.amount.message?.toString()}</div>}
-                  <span className="form-text-des">Balance: {_amount?.toString()} Aura</span>
+                  {errors.amount && (
+                    <div className="form-error">
+                      {errors.amount.message?.toString()}
+                    </div>
+                  )}
+                  <span className="form-text-des">
+                    Balance: {_amount?.toString()} {_symbol?.toString()}
+                  </span>
                 </div>
                 <div className="mb-9">
-                  <button type="submit" className="button-border-gradient bg-brand-gradient" disabled={isPending || isConfirming}>
-                    <div className="button-border-gradient-inside">{isPending ? "Confirming..." : isConfirming ? "Waiting for confirmation..." : "Deposit"}</div>
+                  <button
+                    type="submit"
+                    className="button-border-gradient bg-brand-gradient"
+                    disabled={isPending || isConfirming}
+                  >
+                    <div className="button-border-gradient-inside">
+                      {isPending
+                        ? "Confirming..."
+                        : isConfirming
+                        ? "Waiting for confirmation..."
+                        : "Deposit"}
+                    </div>
                   </button>
                   {/* {isConfirming && (
                     <div className="text-noti mt-2">
@@ -224,11 +309,32 @@ function Deposit() {
                     </div>
                   )} */}
                   {/* {isConfirmed && <div>Transaction confirmed.</div>} */}
-                  {error && <div>Error: {(error as BaseError).shortMessage || error.message}</div>}
-                  <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark"></ToastContainer>
+                  {error && (
+                    <div>
+                      Error:{" "}
+                      {(error as BaseError).shortMessage || error.message}
+                    </div>
+                  )}
+                  <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="dark"
+                  ></ToastContainer>
                 </div>
               </form>
-              <div className="text-tutorial">Note: Direct deposit AURA to CEXes through the Hex address format is under construction, thus this page exists. Your AURA will be sent to an address that is operated by Aura Network, and we will sent it to your deposit address.</div>
+              <div className="text-tutorial">
+                Note: Direct deposit AURA to CEXes through the Hex address
+                format is under construction, thus this page exists. Your AURA
+                will be sent to an address that is operated by Aura Network, and
+                we will sent it to your deposit address.
+              </div>
             </div>
           </div>
         </div>
