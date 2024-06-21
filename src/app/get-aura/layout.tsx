ffaf493bf4f5cf740293cfe6f45ab5e0158acabe
CloudCludfore <CloudCludfore@gmail.com>
2024-06-21 11:27:19 +0700

    update fix wallet connect evm

--- src/app/get-aura/layout.tsx
+++ src/app/get-aura/layout.tsx
@@ -3,10 +3,11 @@ import { aura } from "@/common/aura-chain";
 import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
-import WalletConnectEVM from "./wallet-connect";
 import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import dynamic from "next/dynamic";
+const WalletConnectEVM = dynamic(() => import("./wallet-connect"), { ssr: false });
 // export const metadata: Metadata = {
 //   title: "Get Aura with Aura Network",
 //   description: "",
@@ -17,7 +18,6 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   return (
     <div className="bg-[#000] pb-24">
       <RainbowKitProvider>
