ab34194ffb320e540c10e964e07f175864453c20
DESKTOP-APEQLA2\NOSEL <sonln4793@gmail.com>
2024-06-21 11:29:09 +0700

    update

--- src/app/get-aura/wallet-connect.tsx
+++ src/app/get-aura/wallet-connect.tsx
@@ -23,14 +23,35 @@ function WalletConnectEVM() {
       <div className="main-container">
         <div className="flex flex-col items-center gap-6 lg:gap-16 mt-[14px]">
           <div className="xl:max-w-[867px] flex flex-col gap-10 px-2 items-center">
-            {!isConnected ? (
-              <div className="flex flex-col">
-                <h1 className="text-white self-stretch text-center lg:text-center text-[36px] lg:text-[48px] font-semibold leading-[52px] lg:leading-[80px] font-ppmori-semibold">Your Journey Start Here</h1>
-                <div className="text-medium-gray text-center text-xl lg:text-[20px] font-normal leading-8 max-w-[717px]">But first, let’s get you some AURA</div>
+            {!isConnected && (
+              <div className="flex flex-col gap-10 items-center">
+                <div className="flex flex-col">
+                  <h1 className="text-white self-stretch text-center lg:text-center text-[36px] lg:text-[48px] font-semibold leading-[52px] lg:leading-[80px] font-ppmori-semibold">
+                    Your Journey Start Here
+                  </h1>
+                  <div className="text-medium-gray text-center text-xl lg:text-[20px] font-normal leading-8 max-w-[717px]">
+                    But first, let’s get you some AURA
+                  </div>
+                </div>
+                <ConnectButton.Custom>
+                  {({ account, openConnectModal }) => {
+                    return (
+                      <button
+                        onClick={openConnectModal}
+                        className={`w-fit rounded-full bg-brand-gradient hover:text-[#FD9552] hover:bg-dark-charcoal hover:bg-none hover:border hover:border-[#262320] py-4 px-8 font-semibold text-black text-lg leading-normal h-[54px] flex items-center`}
+                      >
+                        Connect wallet
+                      </button>
+                    );
+                  }}
+                </ConnectButton.Custom>
               </div>
-            ) : (
+            )}
+            {isConnected && (
               <div className="flex flex-col">
-                <h1 className="text-white self-stretch text-center lg:text-center text-[36px] lg:text-[80px] font-semibold leading-[52px] lg:leading-[80px] font-ppmori-semibold">Welcome</h1>
+                <h1 className="text-white self-stretch text-center lg:text-center text-[36px] lg:text-[80px] font-semibold leading-[52px] lg:leading-[80px] font-ppmori-semibold">
+                  Welcome
+                </h1>
                 <div className="flex gap-8 items-center justify-center">
                   <div className="address">{address}</div>
                   <Image
@@ -47,17 +68,6 @@ function WalletConnectEVM() {
                   <Tooltip id="disconnect" />
                 </div>
               </div>
-            )}
-            {!isConnected && (
-              <ConnectButton.Custom>
-                {({ account, openConnectModal }) => {
-                  return (
-                    <button onClick={openConnectModal} className={`w-fit rounded-full bg-brand-gradient hover:text-[#FD9552] hover:bg-dark-charcoal hover:bg-none hover:border hover:border-[#262320] py-4 px-8 font-semibold text-black text-lg leading-normal h-[54px] flex items-center`}>
-                      Connect wallet
-                    </button>
-                  );
-                }}
-              </ConnectButton.Custom>
             )}
           </div>
         </div>
